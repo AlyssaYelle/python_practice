1009e019521bf9f0c157f0323542dea701cc1a67
Alyssa <alyssadanijones@gmail.com>
2018-01-10 15:44:27 -0500

    estimating mean and stdev of data from an unknown distribution

--- data_vis/estimates.py
+++ data_vis/estimates.py
@@ -5,3 +5,54 @@ what is eyeball estimate of the mean?
 what is eyeball estimate of var, sd?
 '''
 
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+from matplotlib import colors as mcolors
+
+if __name__ == '__main__':
+	#import data
+	df = pd.read_csv('data/data.csv', header = 0)
+
+	
+	plt.hist(df['data'], color = 'thistle', bins = 20)
+	plt.xlabel('Frequency')
+	plt.ylabel('Data value')
+	plt.axvline(0.25, color = 'mediumvioletred')
+	plt.axvline(2.25, color = 'palevioletred')
+	plt.axvline(-1.75, color = 'palevioletred')
+	plt.axvline(4.25, color = 'pink')
+	plt.axvline(-3.75, color = 'pink')
+	plt.axvline(6.25, color = 'linen')
+	plt.axvline(-5.75, color = 'linen')
+	plt.show()
+	plt.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sampling/gibbs.py
+++ sampling/gibbs.py
@@ -6,3 +6,36 @@ Calculate posteriors for mu and s^2
 write a Gibbs sampler to sample the joint posterior distribution over m u and s^2
 draw 300 samples with 100 burn-in and plot a scatter plot of mu vs s^2
 '''
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+from matplotlib import colors as mcolors
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
