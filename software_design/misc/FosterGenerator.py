04739d7702a0ad61e44380c8aae05bb5129870d4
Alyssa <alyssadanijones@gmail.com>
2020-01-27 11:39:59 -0500

    made pet class abstract and handled input errors in base survey. TODO: handle input errors in cat and dog surveys

--- software_design/misc/FosterGenerator.py
+++ software_design/misc/FosterGenerator.py
@@ -1,4 +1,6 @@
 '''
+Written and tested in Python 3.7 by Alyssa Jones
+
 Program to generate a foster animal after user inputs information about their household
 '''
 
@@ -6,7 +8,7 @@ import math
 import random
 
 
-# Pet super class
+# abstract Pet class
 class Pet:
 
 	# constructor
@@ -22,47 +24,67 @@ class Pet:
 		return age + " " + scale
 
 
-	# placeholder speak method -- should be updated in child class
+	# require speak method to be defined in child classes
 	def speak(self):
-		# TODO
-		# raise not implemented error
-		print("Hi there!")
+		raise NotImplementedError
 
+	# require describe_self method to be defined in child classes
+	def describe_self(self):
+		raise NotImplementedError
+
+	# method to make pet speak and describe themself to user
 	def greet(self):
 		self.speak()
-		print("I am a " + self.size_descriptive + " " + self.describe_age() + " " + self.age_descriptive + ".")
+		self.describe_self()
+
 
 
 # Dog class is child of Pet class
 class Dog(Pet):
 
-	# constructor
-	def __init__(self, age, weight):
-		# adding on to pet constructor
-		super().__init__(age, weight)
-		self.size_descriptive = "large" if weight >= 50 else "small"
-		self.age_descriptive = "dog" if age >= 1 else "puppy"
-
-
+	# method to make dog 'speak' (make a noise)
 	def speak(self):
+
+		# Adult dogs (>= 1 yr old) say 'woof'
+		# Puppies (< 1 yr old) say 'arf'
 		speak = "Woof!" if self.age >= 1 else "Arf!"
+
 		print(speak)
+
+	# method to describe dog based on age and weight
+	def describe_self(self):
+
+		# large dog defined by weight >= 50
+		size_descriptive = "large" if self.weight >= 50 else "small"
+
+		# adult dog defined by age >= 1
+		age_descriptive = "dog" if self.age >= 1 else "puppy"
+
+		print("I am a " + size_descriptive + " " + self.describe_age() + " " + age_descriptive + ".")
 
 
 # Cat class is child of Pet class
 class Cat(Pet):
 
-	# constructor
-	def __init__(self, age, weight):
-		# adding on to pet constructor
-		super().__init__(age, weight)
-		self.size_descriptive = "chonky" if weight >= 14 else "healthy"
-		self.age_descriptive = "cat" if age >= 1 else "kitten"
-
-
+	# method to make cat 'speak' (make a noise)
 	def speak(self):
+
+		# Adult cats (age >= 1) say 'meoww'
+		# Kittens (age < 1) say 'mew'
 		speak = "Meowww!" if self.age >= 1 else "Mew!"
+
 		print(speak)
+
+	# method to describe cat based on age and weight
+	def describe_self(self):
+
+		# chonky cat defined by weight > 14
+		size_descriptive = "chonky" if self.weight > 14 else "healthy"
+
+		# adult cat defined by age >= 1
+		age_descriptive = "cat" if self.age >= 1 else "kitten"
+
+		print("I am a " + size_descriptive + " " + self.describe_age() + " " + age_descriptive + ".")
 
 
 # Application class
@@ -72,16 +94,34 @@ class Application:
 		self.cats_OK = False
 		self.foster_specs = []
 
+	# method for basic survey to determine interest in dogs and/or cats
 	def take_survey(self):
+
 		# check if dogs are OK
-		self.dogs_OK = True if (input("Are you open to fostering a dog? (y/n): ") == "y") else self.dogs_OK
+		open_to_dogs = input("Are you open to fostering a dog? (y/n): ")
+
+		# handle invalid input
+		while open_to_dogs not in ["y", "Y", "n", "N"]:
+			print("Invalid input. Please enter y or n")
+			open_to_dogs = input("Are you open to fostering a dog? (y/n): ")
+
+		# update self.dogs_OK if user enters y or Y
+		self.dogs_OK = True if open_to_dogs in ["y", "Y"] else self.dogs_OK
 
 		# only display dog survey if dogs are OK
 		if self.dogs_OK == True:
 			self.dog_survey()
 
 		# check if cats are OK
-		self.cats_OK = True if (input("Are you open to fostering a cat? (y/n): ") == "y") else self.cats_OK
+		open_to_cats = input("Are you open to fostering a cat? (y/n): ")
+
+		# handle invalid input
+		while open_to_cats not in ["y", "Y", "n", "N"]:
+			print("Invalid input. Please enter y or n")
+			open_to_cats = input("Are you open to fostering a cat? (y/n): ")
+
+		# update self.cats_OK if user enters y or Y
+		self.cats_OK = True if open_to_cats in ["y", "Y"] else self.cats_OK
 
 		# only display cat survey if cats are OK
 		if self.cats_OK == True:
@@ -92,6 +132,7 @@ class Application:
 			print("\nUnfortunately we only have cats and dogs at this time! Please check back later. :)")
 			return
 
+		# review survey
 		self.survey_results()
 
 	def dog_survey(self):
@@ -137,6 +178,8 @@ class Application:
 		if len(self.foster_specs) == 0:
 			print("We cannot approve your application at this time. Please review our foster requirements and check back later! :)\nCat requirements: Foster must keep cat indoors at all times.\nDog requirements: Foster must commit to walking dog at least 3 times per day.")
 			return
+		else:
+			print("\nCongratulations! Meet your new foster pet :)\n")
 
 		self.generate_foster_animal()
 
@@ -164,8 +207,12 @@ if __name__ == "__main__":
 	print("Welcome to Alyssa's Furry Friend Rescue! \n")
 	# my_dog = Dog(5, 80)
 	# my_dog.greet()
+	# my_cat = Cat(0.2, 4)
+	# my_cat.greet()
 	app = Application()
 	app.take_survey()
+	# pet = Pet(4, 20)
+	# pet.greet()
 
 
 
