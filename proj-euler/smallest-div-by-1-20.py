73aa30220d7e537a065a0afb5419c1d5affd143a
Alyssa <alyssadanijones@gmail.com>
2020-01-29 13:24:33 -0500

    found more efficient solution

--- proj-euler/smallest-div-by-1-20.py
+++ proj-euler/smallest-div-by-1-20.py
@@ -4,94 +4,40 @@ written & tested in Python 3.7 by Alyssa Jones
 
 problem: find the smallest positive integer that is divisible by all integers between 1 & 20, inclusive
 
-algo:
-
-FOR each integer (int) between 2 & 20 (inclusive)
-	IF (int) is prime
-		power n = 1, ...
-		raise it by one power and append (int) to list again while (int)^n <= 20
-
-MULTIPLY primes in prime list together to get minimum integer divisible by all in [1, ..., 20]
-
 '''
+
 import math
 
-def isPrime(my_int):
+def get_primes(stopping_point):
 
-	if my_int == 2:
-		return True
-	# other even nums are not prime
-	elif my_int % 2 == 0:
-		return False
-
-	# if my_int mod any other integer is 0 then it is not prime
-	for i in range(3, my_int // 2):
-		if my_int % i == 0:
-			return False
-
-	# if we get to this point, my_int must be prime
-	return True
-
-# UNNECESSARY FOR THIS PROBLEM
-def primeFactorization(my_int):
-
-	# if int is prime return just the int
-	if isPrime(my_int) == True:
-		return [my_int]
-
-	# empty list to store prime factorization
-	prime_factorization = []
-
-	# begin checking if 2 & my_int // 2 are factors
-	int1 = 2
-	int2 = math.floor(my_int // 2)
-
-	found_factors = False
-
-	# check if int1 * int2 = my_int until success
-	while (int1 <= int2) & (found_factors == False):
-
-		if int1 * int2 == my_int:
-			found_factors = True
-			factors = [int1, int2]
-
-			# recursively find prime factorization of the found factors
-			for factor in factors:
-				if isPrime(factor) == True:
-					prime_factorization.append(factor)
-				else:
-					sub_factors = primeFactorization(factor)
-
-					for factor in sub_factors:
-						prime_factorization.append(factor)
-
-		# increment up int1 (if it is not a factor of my_int) and increment down int2 (if it is not a factor of my_int)
-		else:
-			if my_int % int1 == 0:
-				int2 -= 1
-			elif my_int % int2 == 0:
-				int1 += 1
-			else:
-				int1 += 1
-				int2 -= 1
-
-	# return list of prime
-	return prime_factorization
-
-def get_minimum_primes(max):
 	primes = []
 
-	for i in range(2, max + 1):
-		if isPrime(i) == True:
-			pow = 1
-			while (i**pow) <= max:
+	for i in range(2, stopping_point + 1):
+
+		maybePrime = True
+
+		for prime in primes:
+			if i % prime == 0:
+				maybePrime = False
+				break 
+
+		if maybePrime:
+			power = 1
+
+			while i**power <= stopping_point:
 				primes.append(i)
-				pow += 1
+				power += 1
 
 	return primes
 
+
+
+
 def compute_smallest_div_integer(max):
-	primes = get_minimum_primes(max)
+	if max < 1:
+		raise ValueError
+
+	primes = get_primes(max)
 
 	product = 1
 
@@ -102,8 +48,9 @@ def compute_smallest_div_integer(max):
 
 if __name__ == "__main__":
 
-	min_int = compute_smallest_div_integer(20)
-	print(min_int)
+
+	product = compute_smallest_div_integer(20)
+	print(product)
 
 
 
